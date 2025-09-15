bca79fe1de91799e8db870f230527f88fb535df4
olavo-goes <olavo.goes@estudante.ifms.edu.br>
2025-09-15 10:27:58 -0400

    login e cadastro funcionando

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const UserController = require("./Controllers/UserController");
 const app = express();
+const cors = require("cors");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.get("/areaLogada",UserController.verificarAutenticacao, (req, res) => {
     res.json({
---
+++ frontend-novo/src/pages/AreaLogada.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AreaLogada() {
+  return (
+    <div>
+      <h1>Área logada</h1>
+      <p>Você realizou o login</p>
+    </div>
+  );
+}
+
+export default AreaLogada;
