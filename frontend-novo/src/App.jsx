52c788982582ab7389ef3429fa358cf2a3460900
Marina982 <marina.goes@estudante.ifms.edu.br>
2025-10-21 16:07:22 -0300

    front

--- frontend-novo/src/App.jsx
+++ frontend-novo/src/App.jsx
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
 import AreaLogada from "./pages/AreaLogada";
+import Perfil from "./pages/Perfil";
+import ListaPerfis from "./pages/ListaPerfis";
+import CadastroMesa from "./pages/CadastroMesa";
 
 
 
@@ -13,6 +16,9 @@ function App () {
         <Route path="/cadastro" element={<Cadastro />}/>
         <Route path="/" element={<Login />}/>
         <Route path="/area-logada" element={<AreaLogada />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/perfis" element={<ListaPerfis />} />
+        <Route path="/CadastroMesas" element={<CadastroMesa />} />
       </Routes>
     </Router>
   );
